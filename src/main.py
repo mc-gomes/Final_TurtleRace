2a5225d76cfc3b542f21cca2c50d22aafebac0ff
mc-gomes <bigmath2001@hotmail.com>
2023-02-06 11:25:14 -0300

    adição de novo corredor; início da disputa

--- src/main.py
+++ src/main.py
@@ -1,40 +1,54 @@
 import tkinter as tk
-import turtle
-from turtle import ScrolledCanvas, RawTurtle, TurtleScreen
+from turtle import RawTurtle
 from time import sleep
-import random
+from random import randint
 
 
 CVWIDTH = 650
 CVHEIGHT = 500
 
+# JANELA PRINCIPAL
 root = tk.Tk()
 root.title('Turtle Race')
 root.config(bg='#f2f0eb')
 
-
+# Frame com a mensagem para a escolha do palpite
 frameescolhas = tk.Frame(root, pady=20)
 frameescolhas.grid(row=0, column=0, padx=10, pady=10)
 
-tk.Label(frameescolhas, text="""Escolha entre as opções disponíveis, 
+tk.Label(frameescolhas, text="""Quem você acha que vai ganhar?
+ Dê o seu palpite entre as opções disponíveis, 
  em seguida inicie o jogo.""").pack(side="top", pady=15)
 
+# Tela da área da corrida
 canvas = tk.Canvas(root, width=CVWIDTH, height=CVHEIGHT)
 canvas.configure(bg='green')
 canvas.grid(row=0, column=1, padx=10, pady=10)
 
-screen = TurtleScreen(canvas).bgcolor('green')
+# INICIALIZAÇÃO DOS OBJETOS TURTLE
+
+# Turtle para marcar a linha de partida
 t_linha_start = RawTurtle(canvas)
 t_linha_div = RawTurtle(canvas)
 t_linha_div.ht()
 
+# Turtle para gerar os pontos no mapa
 t_pontos = RawTurtle(canvas)
 t_pontos.ht()
 
-tartuga = RawTurtle(canvas)
-tartuga.ht()
-tartuga.color("blue")
-tartuga.speed(1)
+# Turtle de corrida 1
+t1 = RawTurtle(canvas)
+t1.shape('turtle')
+t1.ht()
+t1.color("orange")
+t1.speed(1)
+
+# Turtle de corrida 2
+t2 = RawTurtle(canvas)
+t2.shape('turtle')
+t2.ht()
+t2.color("blue")
+t2.speed(1)
 
 
 def gerar_linhas():
@@ -60,6 +74,7 @@ def gerar_linha_chegada():
     espaco = 20
     t_linha_chegada = RawTurtle(canvas)
     t_linha_chegada.shape('square')
+    t_linha_chegada.ht()
     t_linha_chegada.up()
     t_linha_chegada.speed(4)
 
@@ -82,36 +97,54 @@ def gerar_linha_chegada():
         t_linha_chegada.stamp()
 
 
-def gerar_pontos(cor: str):
+def gerar_pontos(cor: str, y):
     t_pontos.ht()
     t_pontos.color(cor)
-    t_pontos.speed(1)
+    t_pontos.speed(2)
 
     coord = []
 
-    for _ in range(10):
-        x = random.randint(-200, 200)
-        y = random.randint(-200, -50)
-        coord.append((x, y))
-        t_pontos.up()
-        t_pontos.setpos(x, y)
-        t_pontos.down()
-        t_pontos.dot(10)
+    if y == 1:
+        for _ in range(10):
+            x = randint(-200, 200)
+            y = randint(50, 220)
+            coord.append((x, y))
+            t_pontos.up()
+            t_pontos.setpos(x, y)
+            t_pontos.down()
+            t_pontos.dot(10)
+
+    if y == 2:
+        for _ in range(10):
+            x = randint(-200, 200)
+            y = randint(-220, -50)
+            coord.append((x, y))
+            t_pontos.up()
+            t_pontos.setpos(x, y)
+            t_pontos.down()
+            t_pontos.dot(10)
 
     return coord
 
 
 def set_posicoes():
-    tartuga.up()
-    tartuga.speed(4)
-    tartuga.setpos(-270, -125)
-    tartuga.speed(1)
+    t1.up()
+    t1.speed(4)
+    t1.setpos(-270, 125)
+    t1.speed(1)
+
+    t2.up()
+    t2.speed(4)
+    t2.setpos(-270, -125)
+    t2.speed(1)
 
 
 def inicializar():
     t_pontos.clear()
-    tartuga.st()
-    tartuga.clear()
+    t1.st()
+    t1.clear()
+    t2.st()
+    t2.clear()
     set_posicoes()
 
 
@@ -122,61 +155,68 @@ def valendo():
     opt1.config(state='disabled')
     opt2.config(state='disabled')
 
-    tartuga.down()
+    t1.down()
+    t2.down()
 
-    coordenadas = gerar_pontos('red')
+    coordenadas1 = gerar_pontos('red', 1)
+    coordenadas2 = gerar_pontos('purple', 2)
 
     sleep(0.5)
-    for i in range(len(coordenadas)):
-        tartuga.goto(coordenadas[i])
-        sleep(0.7)
+    for i in range(len(coordenadas1)):
+        t1.speed(randint(1, 4))
+        t2.speed(randint(1, 4))
+        t1.goto(coordenadas1[i])
+        t2.goto(coordenadas2[i])
 
-    while tartuga.xcor() <= 250:
-        print(tartuga.xcor())
-        tartuga.forward(10)
+    while t1.xcor() <= 250 and t2.xcor() <= 250:
+        t1.forward(randint(6, 10))
+        t2.forward(randint(6, 10))
+
+    vencedor()
+
     botao_iniciar.config(state='normal')
     botao_reset.config(state='normal')
     opt1.config(state='normal')
     opt2.config(state='normal')
 
 
-def ver_escolha():
-    escolha_user = v.get()
-    return escolha_user
+def vencedor():
 
+    FONT = ("Courier", 18, "bold")
 
-v = tk.StringVar()
-opcoes = ['Azul', 'Verde']
-v.set(opcoes[0])
-
-
-opt1 = tk.Radiobutton(
-    frameescolhas,
-    text=opcoes[0],
-    padx=20,
-    variable=v,
-    command=ver_escolha,
-    value=opcoes[0])
-opt1.pack(anchor=tk.W)
-
-opt2 = tk.Radiobutton(
-    frameescolhas,
-    text=opcoes[1],
-    padx=20,
-    variable=v,
-    command=ver_escolha,
-    value=opcoes[1])
-opt2.pack(anchor=tk.W)
+    if t1.xcor() > t2.xcor():
+        t1.write('Laranja venceu!', font=FONT, align='right')
+    else:
+        t2.write('Azul venceu!', font=FONT, align='right')
 
 
 if __name__ == '__main__':
+    opcoes = {'Laranja': 'orange', 'Azul': 'blue'}
+    v = tk.StringVar(None, opcoes['Laranja'])
+
+    opt1 = tk.Radiobutton(
+        frameescolhas,
+        text=list(opcoes.keys())[0],
+        padx=20,
+        variable=v,
+        value=opcoes['Laranja'])
+    opt1.pack(anchor=tk.W)
+
+    opt2 = tk.Radiobutton(
+        frameescolhas,
+        text=list(opcoes.keys())[1],
+        padx=20,
+        variable=v,
+        value=opcoes['Azul'])
+    opt2.pack(anchor=tk.W)
+
     gerar_linhas()
     inicializar()
 
     framebotao = tk.Frame(root, pady=20)
     framebotao.grid(row=0, column=2, padx=10, pady=10)
 
-    botao_iniciar = tk.Button(framebotao, text="Iniciar trajeto", command=valendo,
+    botao_iniciar = tk.Button(framebotao, text="Iniciar corrida", command=valendo,
                               state='normal')
     botao_iniciar.pack(pady=10)
 
